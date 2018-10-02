7de085f83956d06d4102c78bc87ef4f0e5c6b378
Tien Tran <tientran2407.vn@gmail.com>
2018-10-02 06:52:59 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,7 @@ def first_challenge
   }
 
   #your code here
-contacts["Freddy Mercury"][favorite_icecream_flavors].delete_if do |flavor| value = "strawberry"
+contacts["Freddy Mercury"][favorite_icecream_flavors].delete_if { |flavor| value = "strawberry"}
 
   #remember to return your newly altered contacts hash!
   contacts
